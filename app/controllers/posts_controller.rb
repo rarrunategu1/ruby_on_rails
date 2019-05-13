29b524d17fefd7020c3af5ef1ca4a300f5dcd43c
rarrunategu1 <parker.rose@ymail.com>
2019-05-13 19:33:55 -0400

    used resources for post routes and created a 'new' method in post controller which outputs a form in the view and once submitted the create method shows a simple render just to show that it's getting the new info submitted

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index
-    
+  def index; end
+
+  def new; end
+
+  def create
+    # will render the information entered and submitted in the new form
+    render plain: params[:post].inspect
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  #root
+
+  # root
   root 'posts#index'
 
-  #other routes
+  # other routes
   get 'about' => 'pages#about'
+
+  # maps HTTP verbs to controller actions
+  resources :posts
 end
