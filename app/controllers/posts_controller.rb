7783ff37cb187adf0af005d188bb14ad32b723a3
rarrunategu1 <parker.rose@ymail.com>
2019-05-13 20:11:40 -0400

    created a post model and migrated it, show method get the new saved blog post and renders it in show views while the index method shows all the posts at submitted using a for loop in the index.html.erb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
-  def index; end
-
+  def index
+    #show all the posts
+    @posts = Post.all
+  end
+    
   def new; end
+
+  def show
+    # will get the single post from create
+    @post = Post.find(params[:id])
+  end
 
   def create
     # will render the information entered and submitted in the new form
-    render plain: params[:post].inspect
+    # render plain: params[:post].inspect
+
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
+
+    # private method tells it that we want to allow it to have a title and a body
   end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
+    end
 end
