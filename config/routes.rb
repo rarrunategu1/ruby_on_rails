e5489e1efcd658f2f051784acf49ae347abead1d
rarrunategu1 <parker.rose@ymail.com>
2019-05-14 18:03:15 -0400

    labeled root as home in the routes and added a navigation bar with nav links to Home and create post

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # root
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   # other routes
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   # maps HTTP verbs to controller actions
   resources :posts
